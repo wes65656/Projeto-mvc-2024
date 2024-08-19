b298ef6fa7e3e4025dfee83fe78e2bdae1264048
wes65656 <71239474+wes65656@users.noreply.github.com>
2024-08-18 22:44:56 -0300

    Att projeto

---
+++ Projeto_2024/Models/Categoria.cs
@@ -0,0 +1,8 @@
+namespace Projeto_2024.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+}
